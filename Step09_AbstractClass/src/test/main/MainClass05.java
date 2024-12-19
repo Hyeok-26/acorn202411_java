9216c5453338bf1698cb8def8c0abddba44a99fd
Noh Hyeokcheon <gurcjs0132@naver.com>
2024-12-19 16:59:09 +0900

    Step09 MainClass05 에 주석 추가

--- Step09_AbstractClass/src/test/main/MainClass05.java
+++ Step09_AbstractClass/src/test/main/MainClass05.java
@@ -4,7 +4,7 @@ import test.mypac.Weapon;
 
 public class MainClass05 {
 	public static void main(String[] args) {
-		//
+		//익명의 innerClass를 이용해서 Weapon type 의 참조값을 얻어내서 변수에 담은 다음
 		Weapon w1 = new Weapon() {//class ? extends Weapon{   }
 			//이름이 없는 Weapon class 를 상속받은 local inner 클래스를 만들어 
 			//추상 클래스 타입(new Weapon())의 참조값을 가져온다
@@ -13,9 +13,11 @@ public class MainClass05 {
 				System.out.println("정수리 10연타");
 			}
 		};
+		//메소드 호출하면서 전달하기
 		useWeapon(w1);
+		
+		//메소드 호출하면서 즉석에서 Weapon type 의 참조값을 얻어내서 전달하기
 		useWeapon(new Weapon() {
-			
 			@Override
 			public void attack() {
 				System.out.println("김구라를 공격하라!");
