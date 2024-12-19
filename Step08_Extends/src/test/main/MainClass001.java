fe04a7a34189649ea40958aa16a8954ef3546737
Noh Hyeokcheon <gurcjs0132@naver.com>
2024-12-19 11:08:53 +0900

    상속자

--- Step08_Extends/src/test/main/MainClass001.java
+++ Step08_Extends/src/test/main/MainClass001.java
@@ -1,9 +1,25 @@
 package test.main;
 
+import test.auto2.MyCar;
+import test.auto.Engine;
+import test.mypac.AndroidPhone;
+import test.mypac.HandPhone;
+import test.mypac.IosPhone;
 import test.mypac.Phone;
 
 public class MainClass001 {
 	public static void main(String[] args) {
-		MainClass08.usePhone(new Phone());
+		Phone p = new Phone();
+		Phone p1 = new HandPhone();
+		Object o = new Phone();
+		HandPhone ip1 = (HandPhone)p1;
+		
+		Engine e = new Engine();
+		MyCar car1 = new MyCar(new Engine());
+		car1.drive();
+		car1.myDrive();
+		
 	}
+	
+	
 }
